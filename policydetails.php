b647a9ea632467cd6142fe56d069c22625a06c99
Ketan2010 <57591621+Ketan2010@users.noreply.github.com>
2020-01-18 09:37:43 +0530

    Update policydetails.php

--- policydetails.php
+++ policydetails.php
@@ -62,31 +62,42 @@
       </div>
     </div>
   </div>
-  <div class="card">
+ 
+   <div class="card">
     <div class="card-header" id="headingTwo">
       <h2 class="mb-0">
         <button class="btn btn-primary btn-lg active" class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          Collapsible Group Item #2
+          Health Optima
         </button>
       </h2>
     </div>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-primary btn-lg active" class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      <div class="card-body" >
+         Health Optima provides a variety of Health Insurance Plans & Mediclaim Policies that offer financial security to meet health related contingencies. Due to changing lifestyles, health issues have not just escalated, they have increasingly become more complex in nature. It becomes imperative therefore to have a health insurance plan in place, thus your financial planning is incomplete if you have not accounted for health.
+             <table class="table table-bordered">
+            <tbody>
+                <tr>
+                  <th scope="row">Installment</th>
+                  <td>6409 +GST</td>
+                
+                </tr>
+                <tr>
+                  <th scope="row">Period (years)</th>
+                  <td>2</td>
+                
+                </tr>
+                <tr>
+                  <th scope="row">Amount ( lakh ) </th>
+                  <td>2</td>
+        
+                </tr>
+                 <tr>
+                  <th scope="row">Validity (years) </th>
+                  <td>3</td>
+                
+                </tr>
+              </tbody>
+            </table>
       </div>
     </div>
   </div>
